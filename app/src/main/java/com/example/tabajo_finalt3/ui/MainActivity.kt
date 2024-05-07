370205bf3a9b0b25981ad45d81f7cff39eb56dd3
Jose Manuel Borras Rodriguez <josemanuel.borras@esceuelaestech.es>
2024-05-07 11:42:30 +0200

    first commit

---
+++ app/src/main/java/com/example/tabajo_finalt3/ui/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.tabajo_finalt3.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavHost
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.tabajo_finalt3.R
+import com.example.tabajo_finalt3.databinding.MainActivityBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: MainActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.navContainer) as NavHost
+        val navController = navHost.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+   //     setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navContainer)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
