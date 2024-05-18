b7d802facb3033f1c5fc1c0a9bd6e013d2558ab7
Daniel <88139222+LordTenebrous@users.noreply.github.com>
2024-05-19 00:03:27 +0200

    buscador de ingredientes

---
+++ app/src/main/java/com/example/trabajo_final_t3/ui/fragments/DetailRecipeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.trabajo_final_t3.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.trabajo_final_t3.R
+
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class DetailRecipeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_recipe, container, false)
+    }
+
+
+}
